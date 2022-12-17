58ffae3cf9cac15ab51f416e890903529ceafc1b
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-12-17 21:40:31 +0530

    added animations

--- StreamHack/Assets/Scripts/PlayerMovement.cs
+++ StreamHack/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D coll;
+    private Animator animator;
+    private SpriteRenderer sprite;
+    
+    private float playerDirectionX = 0f;
+
+    private enum MovementState { idle, running, jumping, doubleJumping, falling};
+
     [SerializeField] private LayerMask jumpableGround;
 
     // Start is called before the first frame update
@@ -13,19 +20,63 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float playerDirectionX = Input.GetAxisRaw("Horizontal");
+        playerDirectionX = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = new Vector2(playerDirectionX * 7f,rb.velocity.y);
-        Debug.Log(IsGrounded());
+
+
+        // Debug.Log(IsGrounded());
+        // if(IsGrounded() && !Input.GetButtonDown("Jump")){
+        //     doubleJump = false;
+        // }
+
+        // if(Input.GetButtonDown("Jump")){
+        //     if(IsGrounded() || doubleJump){
+        //         rb.velocity = new Vector2(rb.velocity.x,16f);
+        //         doubleJump = !doubleJump;
+        //     }
+        // }
+
         if (Input.GetButtonDown("Jump") && IsGrounded()){
             rb.velocity = new Vector2(rb.velocity.x,14f);
         }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation(){
+
+        MovementState state;
+
+        if(playerDirectionX > 0f){
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if(playerDirectionX < 0f){
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else{
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f){
+            state = MovementState.jumping;
+        }
+        else if(rb.velocity.y < -.1f){
+            state = MovementState.falling;
+        }
+
+        animator.SetInteger("movementState",(int)state);
+
     }
 
     private bool IsGrounded(){
